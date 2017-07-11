4bbe7e2c79249e2cab14af4afdb0f14ef2203229
Pradipta Das <Praddy@pradiptas-macbook-pro.local>
2017-07-12 01:02:36 +0530

    Initial Commit

---
+++ SampleClearingScreen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SampleClearingScreen
+//
+//  Created by Pradipta Das on 12/07/17.
+//  Copyright © 2017 Pradipta Das. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SampleClearingScreen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SampleClearingScreen
+//
+//  Created by Pradipta Das on 12/07/17.
+//  Copyright © 2017 Pradipta Das. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
